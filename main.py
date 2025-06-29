228e47fca699e53893b5f9cf2ddfb8df118ceef1
Lyna Beraich <lynab1@umbc.edu>
2025-06-29 18:07:28 -0400

    fix host and port for render

--- main.py
+++ main.py
@@ -80,4 +80,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=True)
